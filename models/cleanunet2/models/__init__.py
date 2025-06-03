72161fbafa63fa9508a34001ce8fa47bc72d1235
emilybederov <151040825+EmilyBederov@users.noreply.github.com>
2025-06-03 22:08:01 +0300

    Create __init__.py

---
+++ models/cleanunet2/models/__init__.py
@@ -0,0 +1,6 @@
+# models/cleanunet2/models/__init__.py
+from .cleanspecnet import CleanSpecNet
+from .cleanunet import CleanUNet
+from .cleanunet2 import CleanUNet2
+
+__all__ = ['CleanSpecNet', 'CleanUNet', 'CleanUNet2']
